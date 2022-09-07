7c4dfdac493f10028f3f822b49da4fb2ba739fd6
Simon Ljungström <simonlj8@gmail.com>
2022-09-07 11:51:41 +0200

    Add private/protected

--- src/main/java/edu/lernia/labb3/Patient.java
+++ src/main/java/edu/lernia/labb3/Patient.java
@@ -4,40 +4,42 @@ import java.lang.String;
 
 
 public class Patient {
-    boolean isSick = true;
-    String name;
-
-    String getSickness;
-    String sickness = null;
-    // Constructor 1
-    Patient(String name, String getSickness) {
+	
+   protected boolean isSick = true;
+   private String name;
+   private String getSickness;
+    
+    
+    // Constructor 1 for disease and name
+   protected Patient(String name, String getSickness) {
         this.name = name;
         this.getSickness = getSickness;
+        
     }
 
-    // Constructor 2
-    public Patient(String name) {
+    // Constructor 2 only for name
+    protected Patient(String name) {
         this.name = name;
         isSick = false;
     }
 
-    public boolean isSick() {
+    protected boolean isSick() {
         if(getSickness == null) {
             isSick = false;
         }
        return isSick;
    }
 
-    public String getName() {
+   protected String getName() {
         return name;
     }
 
-    public String getSickness() {
+    protected String getSickness() {
         return getSickness;
     }
 
-    public void takeMedication (Medicine medicine){
-    if(getSickness != null && getSickness.equalsIgnoreCase(medicine.getTreatmentName())) {
+    protected void takeMedication (Medicine medicine){
+    if(getSickness != null && getSickness.equals(medicine.getTreatmentName())) {
         isSick = false;
 
           }
